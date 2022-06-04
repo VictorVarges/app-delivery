941218cfb6b6289fb3a603a4a70945cdfbceedea
Adadezer <adadezer@gmail.com>
2022-06-04 12:35:55 -0300

    feat: provider, context

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Routs from './router/Routs';
+import Provider from './context/deliveryProvider';
 
 function App() {
   return (
-    <Routs />
+    <Provider>
+      <Routs />
+    </Provider>
   );
 }
 
