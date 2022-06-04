c12679b33fd74fdcc7f63423348d82c9a4d31dba
Adadezer <adadezer@gmail.com>
2022-06-04 20:38:26 -0300

    feat: screen products

--- front-end/src/page/Products.jsx
+++ front-end/src/page/Products.jsx
@@ -1,6 +1,68 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../components/navbar';
+import datatest from '../util/datatest';
 
 export default function Products() {
-  return (<NavBar />);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const result = await axios.get('http://localhost:3001/customer/products');
+      // console.log('ffffffff', result.data);
+      setProducts(result.data);
+      return result.data;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return (
+    <>
+      <NavBar />
+      {products.map((product, index) => (
+        <div key={ index }>
+          <div>
+            <div data-testid={ `${datatest[16]}${product.id}` }>
+              {product.price}
+            </div>
+            <div>
+              <img
+                data-testid={ `${datatest[17]}${product.id}` }
+                src={ product.url_image }
+                alt=""
+              />
+            </div>
+          </div>
+
+          <div>
+            <div data-testid={ `${datatest[15]}${product.id}` }>
+              {product.name}
+            </div>
+            <div>
+              <button
+                type="button"
+                data-testid={ `${datatest[19]}${product.id}` }
+              >
+                -
+              </button>
+              <input
+                type="text"
+                data-testid={ `${datatest[20]}${product.id}` }
+              />
+              <button
+                type="button"
+                data-testid={ `${datatest[18]}${product.id}` }
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
